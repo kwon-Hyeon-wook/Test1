64d5faa46b16143cd7e0e8e06c5f01cec09b7e71
hyeonwook Kwon <michaelomd@naver.com>
2022-06-09 22:16:38 +0900

    first_commit

---
+++ main.js
@@ -0,0 +1,60 @@
+// let print = names.map((item)=>{return item.toUpperCase()})
+// console.log(print)
+  
+// let print2 = names.map((item)=>{let splitItem = item.split(" ")
+// return splitItem[0]
+// })
+// console.log(print2)
+  
+// let print3 = names.map((item)=>{
+// let splitItem = item.split(' ')
+// let printItem =''
+// splitItem.forEach((word)=>{printItem+=word[0]})
+// return printItem
+// })
+// console.log(print3)
+
+// let print4 = names.filter((item)=> {return item.includes('a')})
+// console.log(print4)
+
+// let print5= names.filter((item)=>{
+// let splitItem = item.split('')
+// return splitItem.some((letter,index)=>{return letter == splitItem[index+1]})})
+// console.log(print5)
+
+
+// let print6 = names.some((item)=>{return item.length>=20})
+// console.log(print6)
+
+// let print7 = names.some((item)=>{let splitItem = item.split(' ')
+// splitItem.pop()
+// return splitItem.some((splitName)=>{return splitName.toLocaleLowerCase().includes('p')})
+// })
+// console.log(print7)
+
+// let print8 = names.every(item=>item.length>=20)
+// console.log(print8)
+
+// let print9 = names.every(item=>item.includes('a'))
+// console.log(print9)
+
+// let print10 = names.find(item=>item.length>=20)
+// console.log(print10)
+
+// let print11 = names.find(item=>item.split(' ').length>2)
+// console.log(print10)
+
+// console.log(names.findIndex(item=>item.length>=20))
+
+// let print12 = names.findIndex(
+// item=>item.split(' ').length>2
+// )
+// console.log(print12)
+
+
+// let print = names.filter(item=>{
+// let splitItem = item.split('')
+// return splitItem.some((letter,index)=>{return letter==splitItem[index+1]})
+// })
+// console.log(print)
+
